c13d3fd35efe07fe95b43ef3e70b0e757bcf82fa
Milos Zarkovic <mzarkovicm@gmail.com>
2015-07-14 02:18:38 +0200

    init

---
+++ index.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>bmw</title>
+    <link rel="stylesheet" type="text/css" href="stil.css">
+  </head>
+  <body>
+    <div>
+      <header>
+        <h1>BMW E30</h1>
+
+        <ul>
+           <li>
+             <a href="index.php">Pocetna</a>
+           </li>
+           <li>
+              <a href="kontakt.html">Kontakt</a>
+           </li>
+        </ul>
+      </header>
+      <section>
+        <aside >
+          <ul>
+             <li>
+               <a href="bmwe30.html">BMW e30 1988</a>
+             </li>
+             <li>
+                <a href="bmw.html">BMW e30 1990</a>
+             </li>
+             <li>
+                <a href="e30.html">BMW e30 1991</a>
+             </li>
+             <li>
+                <a href="m3.html">BMW e30 M3</a>
+             </li>
+          </ul>
+             <img src="img/logo.jpg" width="200"/>
+            <img src="img/benka.jpg" width="200"/>
+            <img src="img/logic.jpg" width="200"/>
+
+
+        </aside>
+        <article>
+          <h1>Changes from the standard 3-series</h1>
+          <iframe width="980" height="540" src="https://www.youtube.com/embed/aRtY3Tg9yJg?rel=0&amp;showinfo=0" frameborder="0" allowfullscreen></iframe>
+          <h2>Body</h2>
+          <p>
+            The E30 M3 differed from the rest of the E30 line-up in many ways. The M3, although using the same basic unit-body shell as the standard E30, was equipped with 12 different and unique body panels for the purposes of improving aerodynamics, as well as "box flared" wheel-arches in the front and rear to accommodate a wider track with wider and taller wheels and tyres. The only exterior body panels the standard model 3-series and the M3 shared were the hood, roof panel, and sunroof.
+          </p>
+          <h2>Suspension</h2>
+          <p>
+            The E30 M3 differs from the standard E30 by having a 5x120 wheel bolt pattern. The E30 M3 had increased caster angle through major front suspension changes. The M3 had specific solid rubber offset control arm bushings. It used aluminum control arms and the front strut tubes were changed to a design similar (bolt on kingpins and swaybar mounted to strut tube) to the E28 5-series. This included carrying over the 5 series front wheel bearings and brake caliper bolt spacing. The rear suspension is a carry over from the E30.
+          </p>
+          <h2>Brakes</h2>
+          <p>
+            The E30 M3 had special front and rear brake calipers and rotors. It also has a special brake master cylinder.
+          </p>
+          <h2>Driveline</h2>
+           <p>
+
+             The E30 M3 had one of two Getrag 265 5-speed gearboxes. US models received an overdrive transmission while European models were outfitted with a dogleg version, with first gear being down and to the left, and fifth gear being a direct 1:1 ratio. Rear differentials installed included a 4.10:1 final-drive ratio for US models. European versions were equipped with a 3.15:1 final drive ratio. All versions were clutch-type limited-slip differentials with 25% lockup.
+          </p>
+
+
+
+
+Wrap up
+<br/>
+          <img src="img/slika.jpg" width="450"/>
+          <img src="img/e30.jpg" width="500"/>
+        </article>
+      </section>
+      <footer>
+        zivac ©2015
+      </footer>
+    </div>
+  </body>
+</html>
